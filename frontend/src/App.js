8e75e10a943379821321b60f52c9c336b0389f3c
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2021-04-03 20:31:24 -0400

    table finesse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as MicrophoneIcon } from "./mic.svg";
 import "./App.css";
 
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+
+
 function App() {
-  const [speaking, setspeaking] = useState(false);
+
+  const [list_of_objects, set_list_of_objects] = useState({ 'House Keys': 'Living Room Table', 'Charger': 'Outlet near the fridge', 'Headphones': 'Desk in the bedroom' });
+  localStorage.setItem('list_of_objects', JSON.stringify(list_of_objects));
+
+  React.useEffect(() => {
+    
+    window.addEventListener('storage', () => {
+      set_list_of_objects(JSON.parse(localStorage.getItem('list_of_objects')) || {})   
+    });
+      
+    }, [])
+
+  const keys = Object.keys(list_of_objects)
 
   return (
-    <React.Fragment>
-      {speaking ? "You're unmuted" : "Click mute"}
-      <button onClick={() => setspeaking(!speaking)}>Bruh</button>
-      <div id="container">
-        <h1>Sam the Helper bot</h1>
-        <MicrophoneIcon />
-      </div>
-    </React.Fragment>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Object</Th>
+            <Th>Location</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+        {keys.map(item => {
+          return (
+            <Tr key={item}>
+              <Td>{item}</Td>
+              <Td>{list_of_objects[item]}</Td>
+            </Tr>
+          );
+        })}
+        </Tbody>
+      </Table>
   );
 }
 
 export default App;
+
